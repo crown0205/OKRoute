66476d2d8f8857d7771f40a028397c80e86a4962
crown0205 <crown0205@gmail.com>
2024-12-21 21:00:19 +0900

    feat: Update GoalMessage component to include prefix in message display

--- src/components/common/Message.tsx
+++ src/components/common/Message.tsx
@@ -16,7 +16,7 @@ function GoalMessage({ children }: PropsWithChildren) {
     <div className="flex flex-row gap-2 items-center">
       💡
       <span className="text-sm whitespace-pre-line break-keep text-[#000]">
-        {children}
+        O : {children}
       </span>
     </div>
   );
