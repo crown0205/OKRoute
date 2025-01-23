fc2794e3309072701117250ece02489a36051e24
crown0205 <crown0205@gmail.com>
2025-01-23 20:46:50 +0900

    feat: Update authentication flow by modifying Kakao login URL and removing unused GitHub, Kakao, and Naver callback routes
    - Changed Kakao login URL to point to local development endpoint.
    - Deleted unused GitHub, Kakao, and Naver authentication callback routes to streamline the codebase.
    - Improved overall authentication management by consolidating relevant logic.

--- src/app/(auth)/Login/page.tsx
+++ src/app/(auth)/Login/page.tsx
@@ -29,7 +29,8 @@ function LoginPage() {
   };
 
   const handleKakaoLogin = () => {
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+    // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = `http://localhost:3001/auth/kakao`;
     window.location.href = KAKAO_AUTH_URL;
   };
 
